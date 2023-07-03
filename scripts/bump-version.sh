9f74f7af8cdb3319b6cb735c2aeaf25db3821efe
felipebrunet <felipe.brunet@gmail.com>
2023-07-03 16:51:18 -0400

    tag version started

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -7,11 +7,12 @@ else
 	new_version=$1
 fi
 
-sed -i -E "s/Version: [0-9]\.[0-9]\.[0-9]/Version: $new_version/" lnbits.php 
+sed -i -E "s/Version: [0-9]\.[0-9]\.[0-9]/Version: $new_version/" lnpagos.php 
 sed -i -E "s/Stable tag: [0-9]\.[0-9]\.[0-9]/Stable tag: $new_version/" readme.txt
 
-git add lnpagos.php
-git add readme.txt
+git add .
+# git add lnpagos.php
+# git add readme.txt
 git commit -m "Bump version to $new_version"
 git tag "v$new_version"
 
