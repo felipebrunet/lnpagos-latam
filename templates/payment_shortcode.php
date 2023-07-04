efc0ac31cfad791938f9fe673a7348b275742a05
felipebrunet <felipe.brunet@gmail.com>
2023-07-04 00:22:36 -0400

    better invoice field

--- templates/payment_shortcode.php
+++ templates/payment_shortcode.php
@@ -1,9 +1,10 @@
 <div class="qr_invoice" id="qr_invoice">
 	<img src="<?php echo esc_url("https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=".$invoice."&choe=UTF-8") ?>"/><br/>
 	<p><?php echo $order_detail ?></p>
-	<textarea id="invoice_text"><?php echo esc_textarea($invoice) ?></textarea>
+	<textarea readonly id="invoice_text"><?php echo esc_textarea($invoice) ?></textarea>
+	<br>
+	<button id='invoice_copy_button'>Copiar Lightning Invoice</button>
 </div>
-
 
 <script type="text/javascript">
 	var $ = jQuery;
@@ -11,7 +12,6 @@
 	var order_id = <?php echo esc_attr($order_id) ?>;
 
 	// Periodically check if the invoice got paid
-
 	setInterval(function() {
 			$.post(check_payment_url, {'order_id': order_id}).done(function(data) {
 				var response = $.parseJSON(data);
@@ -23,9 +23,10 @@
 		}, 5000);
 
 	// Copy into clipboard on click
-	$('#qr_invoice').click(function() {
+	$('#invoice_copy_button').click(function() {
 		$('#invoice_text').select();
 		document.execCommand('copy');
+		// alert("Lightning invoice copiado");
 	});
 	
 </script>
